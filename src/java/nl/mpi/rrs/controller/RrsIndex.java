9c65d1e31357cdf5f4135ff054519ff68ee2afc3
Twan Goosen <t.goosen@gmail.com>
2011-03-16 15:33:17 +0000

    Fixed a lot of  code redundancy from RrsIndex2.java

--- src/java/nl/mpi/rrs/controller/RrsIndex.java
+++ src/java/nl/mpi/rrs/controller/RrsIndex.java
@@ -13,7 +13,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import mpi.corpusstructure.CorpusStructureDBImpl;
 import mpi.corpusstructure.UnknownNodeException;
@@ -21,7 +20,6 @@ import nl.mpi.rrs.model.corpusdb.ImdiNode;
 import nl.mpi.rrs.model.date.PulldownGenerator;
 import nl.mpi.rrs.model.errors.ErrorRequest;
 import nl.mpi.rrs.model.errors.ErrorsRequest;
-import nl.mpi.rrs.model.utilities.RrsUtil;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
@@ -33,14 +31,14 @@ import org.apache.log4j.Level;
  */
 public class RrsIndex extends HttpServlet {
 
+    private static Logger logger = Logger.getLogger(RrsIndex.class);
+
     /** Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        Logger logger = Logger.getLogger("RrsIndex");
         logger.setLevel(Level.INFO);
 
         logger.info("RrsIndex: *************** start *****************");
@@ -51,57 +49,38 @@ public class RrsIndex extends HttpServlet {
         logger.info("getRequestURL :" + request.getRequestURL());
         logger.info("getServletPath :" + request.getServletPath());
 
-
-
-        String checkEmailLinkBase = "";
-        String urlFields[] = request.getRequestURL().toString().split("/");
-
-        for (int i = 0; i < urlFields.length - 1; i++) {
-            checkEmailLinkBase += urlFields[i];
-            checkEmailLinkBase += "/";
-        }
-
-        checkEmailLinkBase += "RrsRegistration";
-
-        logger.info("Url RrsRegistration: " + checkEmailLinkBase);
-        request.setAttribute("RrsRegistration", checkEmailLinkBase);
-
         String urlRrsRegistration = request.getContextPath() + "/RrsRegistration";
         request.setAttribute("urlRrsRegistration", urlRrsRegistration);
 
         ErrorsRequest errorsRequest = new ErrorsRequest();
-        String htmlSelectedNodesTable = "";
+        CorpusStructureDBImpl corpusDbConnection = (CorpusStructureDBImpl) this.getServletContext().getAttribute("corpusDbConnection");
+        if(createNodesTable(request, response, corpusDbConnection, errorsRequest, "")){
+            createCalendarDropdowns(request);
+        }
+        dispatchServlet(request, response, errorsRequest);
+    }
 
+    static boolean createNodesTable(HttpServletRequest request, HttpServletResponse response, CorpusStructureDBImpl corpusDbConnection, ErrorsRequest errorsRequest, String htmlSelectedNodesTable) throws IOException, ServletException {
+        logger.debug("RrsIndex2 Context: htmlSelectedNodesTable:" + htmlSelectedNodesTable);
         String[] values = request.getParameterValues("nodeid");
         if (values != null) {
             if (values.length > 0) {
-
-                CorpusStructureDBImpl corpusDbConnection = (CorpusStructureDBImpl) this.getServletContext().getAttribute("corpusDbConnection");
-
                 if (corpusDbConnection == null) {
-
-                    logger.info("RrsIndex: *************** corpusDbConnection == null ");
-
+                    logger.warn("RrsIndex: *************** corpusDbConnection == null ");
                     ErrorRequest errorRequest = new ErrorRequest();
-
                     errorRequest.setErrorFormFieldLabel("Corpus Database");
                     errorRequest.setErrorMessage("Server is down");
                     errorRequest.setErrorValue("");
                     errorRequest.setErrorException(null);
                     errorRequest.setErrorType("CORPUS_DATABASE_DOWN");
-
                     errorsRequest.addError(errorRequest);
                     errorsRequest.setErrorRecoverable(false);
-
-                    dispatchServlet(request, response, errorsRequest);
-                    return;
+                    return false;
                 }
 
                 for (int i = 0; i < values.length; i++) {
                     if (values[i] != null && !(values[i].equalsIgnoreCase(""))) {
-
                         logger.info("RrsIndex: *************** Param nodeId: " + values[i]);
-
                         ImdiNode imdiNode = new ImdiNode();
                         imdiNode.setImdiNodeIdWithPrefix(values[i]);
                         String nodeIdWithPrefix = imdiNode.getImdiNodeIdWithPrefix();
@@ -110,94 +89,67 @@ public class RrsIndex extends HttpServlet {
                                 imdiNode.setImdiNodeName(corpusDbConnection.getNode(nodeIdWithPrefix).getName());
                                 String nodeNameHtml = "paramNodeName_" + i;
                                 String nodeNameValue = imdiNode.getImdiNodeName();
-
                                 String nodeIdHtml = "nodeid";
                                 //paramNodeId_" + i;
                                 String nodeIdValue = values[i];
-
                                 htmlSelectedNodesTable += "<tr>";
                                 htmlSelectedNodesTable += "    <td> <input type=\"text\" name=\"" + nodeIdHtml + "\"  value = \"" + nodeIdValue + "\" readonly=\"readonly\" size=\"30\" /> ";
                                 htmlSelectedNodesTable += "    </td> ";
-                                
                                 htmlSelectedNodesTable += "    <td> <input type=\"text\" name=\"" + nodeNameHtml + "\"  value = \"" + nodeNameValue + "\" readonly=\"readonly\" size=\"50\" /> ";
                                 htmlSelectedNodesTable += "    </td> ";
-                                
                                 htmlSelectedNodesTable += " </tr> ";
-
-
-
                             } catch (UnknownNodeException ex) {
                                 ErrorRequest errorRequest = new ErrorRequest();
-
                                 errorRequest.setErrorFormFieldLabel("Browser selected: Node Id");
                                 errorRequest.setErrorValue(values[i]);
                                 errorRequest.setErrorMessage("Invalid NodeId");
                                 errorRequest.setErrorException("mpi.corpusstructure.UnknownNodeException");
                                 errorRequest.setErrorType("INVALID_NODE_ID");
-
                                 errorsRequest.addError(errorRequest);
                                 errorsRequest.setErrorFromBrowser(true);
                             }
-
-
                         } else {
                             ErrorRequest errorRequest = new ErrorRequest();
-
                             errorRequest.setErrorFormFieldLabel("Browser selected: Node Id");
                             errorRequest.setErrorValue(values[i]);
                             errorRequest.setErrorMessage("Invalid NodeId");
                             errorRequest.setErrorException("mpi.corpusstructure.UnknownNodeException");
                             errorRequest.setErrorType("INVALID_NODE_ID");
-
                             errorsRequest.addError(errorRequest);
                             errorsRequest.setErrorFromBrowser(true);
                         }
-
                     }
                 }
             }
         }
+        logger.debug("RrsIndex: htmlSelectedNodesTable:" + htmlSelectedNodesTable);
+        request.getSession().setAttribute("htmlSelectedNodesTable", htmlSelectedNodesTable);
+        return true;
+    }
 
+
+    static void createCalendarDropdowns(HttpServletRequest request) {
         Calendar cal = Calendar.getInstance();
-
         PulldownGenerator datePullDownToday = new PulldownGenerator(cal);
-
         String pulldownDayOfMonthToday = datePullDownToday.pulldownDayOfMonth("paramRequestFromDateDay");
         String pulldownMonthOfYearToday = datePullDownToday.pulldownMonthOfYear("paramRequestFromDateMonth");
         String pulldownYearToday = datePullDownToday.pulldownYear("paramRequestFromDateYear");
-
-        cal.set(cal.YEAR, cal.get(cal.YEAR) + 1);
-
-        int year = cal.get(cal.YEAR);
+        cal.add(Calendar.YEAR, 1);
+        int year = cal.get(Calendar.YEAR);
         PulldownGenerator datePullDownEnd = new PulldownGenerator(cal);
-
         String pulldownDayOfMonthEnd = datePullDownEnd.pulldownDayOfMonth("paramRequestToDateDay");
         String pulldownMonthOfYearEnd = datePullDownEnd.pulldownMonthOfYear("paramRequestToDateMonth");
         String pulldownYearEnd = datePullDownEnd.pulldownYear("paramRequestToDateYear", year - 1, year + 9);
-
         request.setAttribute("pulldownDayOfMonthToday", pulldownDayOfMonthToday);
         request.setAttribute("pulldownMonthOfYearToday", pulldownMonthOfYearToday);
         request.setAttribute("pulldownYearToday", pulldownYearToday);
-
         request.setAttribute("pulldownDayOfMonthEnd", pulldownDayOfMonthEnd);
         request.setAttribute("pulldownMonthOfYearEnd", pulldownMonthOfYearEnd);
         request.setAttribute("pulldownYearEnd", pulldownYearEnd);
-
-        logger.info("RrsIndex: htmlSelectedNodesTable:" + htmlSelectedNodesTable);
-        //request.setAttribute("htmlSelectedNodesTable", htmlSelectedNodesTable);
-        //this.getServletContext().setAttribute("htmlSelectedNodesTable", htmlSelectedNodesTable);
-        HttpSession session = request.getSession();
-        session.setAttribute("htmlSelectedNodesTable", htmlSelectedNodesTable);
-        
-
-        dispatchServlet(request, response, errorsRequest);
-        return;
     }
 
-    public void dispatchServlet(HttpServletRequest request, HttpServletResponse response, ErrorsRequest errorsRequest)
+    static void dispatchServlet(HttpServletRequest request, HttpServletResponse response, ErrorsRequest errorsRequest)
             throws ServletException, IOException {
-
-        Logger logger = Logger.getLogger("RrsIndex");
         logger.setLevel(Level.INFO);
 
         if (errorsRequest.getSize() > 0) {
@@ -222,19 +174,15 @@ public class RrsIndex extends HttpServlet {
 
 
         } else {
-            //String fromFirstPage = (String) request.getParameter("fromFirstPage");
-            //logger.info("fromFirstPage: " + fromFirstPage);
-
             String uidFromShib = request.getRemoteUser();
-            //if (uidFromShib.equalsIgnoreCase("anonymous") && RrsUtil.isEmpty(fromFirstPage)) {
-            if (uidFromShib.equalsIgnoreCase("anonymous")) {
-                logger.info("RrsIndex: call index.jsp");
+            if (uidFromShib != null && uidFromShib.equalsIgnoreCase("anonymous")) {
+                logger.debug("RrsIndex: call index.jsp");
 
                 RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/view/page/index.jsp");
                 view.forward(request, response);
             } else {
-                
-                logger.info("RrsIndex: call index_2.jsp");
+
+                logger.debug("RrsIndex: call index_2.jsp");
 
                 RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/view/page/index_2.jsp");
                 view.forward(request, response);
@@ -247,6 +195,7 @@ public class RrsIndex extends HttpServlet {
      * @param request servlet request
      * @param response servlet response
      */
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
@@ -256,6 +205,7 @@ public class RrsIndex extends HttpServlet {
      * @param request servlet request
      * @param response servlet response
      */
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
@@ -263,6 +213,7 @@ public class RrsIndex extends HttpServlet {
 
     /** Returns a short description of the servlet.
      */
+    @Override
     public String getServletInfo() {
         return "Resource Request System Start Servlet";
     }
--- src/java/nl/mpi/rrs/controller/RrsIndex2.java
+++ src/java/nl/mpi/rrs/controller/RrsIndex2.java
@@ -6,22 +6,14 @@
 package nl.mpi.rrs.controller;
 
 import java.io.IOException;
-import java.util.Calendar;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import mpi.corpusstructure.CorpusStructureDBImpl;
-import mpi.corpusstructure.UnknownNodeException;
-import nl.mpi.rrs.model.corpusdb.ImdiNode;
-import nl.mpi.rrs.model.date.PulldownGenerator;
-import nl.mpi.rrs.model.errors.ErrorRequest;
 import nl.mpi.rrs.model.errors.ErrorsRequest;
-import nl.mpi.rrs.model.utilities.RrsUtil;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
@@ -33,14 +25,14 @@ import org.apache.log4j.Level;
  */
 public class RrsIndex2 extends HttpServlet {
 
+    private static Logger logger = Logger.getLogger(RrsIndex2.class);
+
     /** Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
-        Logger logger = Logger.getLogger("RrsIndex2");
         logger.setLevel(Level.INFO);
 
         logger.info("RrsIndex2: *************** start *****************");
@@ -51,207 +43,28 @@ public class RrsIndex2 extends HttpServlet {
         logger.info("getRequestURL :" + request.getRequestURL());
         logger.info("getServletPath :" + request.getServletPath());
 
-        String htmlSelectedNodesTable;
-        //String htmlSelectedNodesTable = (String) request.getAttribute("htmlSelectedNodesTable");
-        //logger.info("RrsIndex2: htmlSelectedNodesTable:" + htmlSelectedNodesTable);
-        
-        //htmlSelectedNodesTable = (String) this.getServletContext().getAttribute("htmlSelectedNodesTable");
-        HttpSession session = request.getSession();
-        htmlSelectedNodesTable = (String) session.getAttribute("htmlSelectedNodesTable");
-        logger.info("RrsIndex2 Context: htmlSelectedNodesTable:" + htmlSelectedNodesTable);
-        
-        String checkEmailLinkBase = "";
-        String urlFields[] = request.getRequestURL().toString().split("/");
-
-        for (int i = 0; i < urlFields.length - 1; i++) {
-            checkEmailLinkBase += urlFields[i];
-            checkEmailLinkBase += "/";
-        }
-
-        checkEmailLinkBase += "RrsRegistration";
-
-        logger.info("Url RrsRegistration: " + checkEmailLinkBase);
-        request.setAttribute("RrsRegistration", checkEmailLinkBase);
-
-        String urlRrsRegistration = request.getContextPath() + "/RrsRegistration";
-        request.setAttribute("urlRrsRegistration", urlRrsRegistration);
-
         ErrorsRequest errorsRequest = new ErrorsRequest();
+
+        CorpusStructureDBImpl corpusDbConnection = (CorpusStructureDBImpl) this.getServletContext().getAttribute("corpusDbConnection");
+        String htmlSelectedNodesTable = (String) request.getSession().getAttribute("htmlSelectedNodesTable");
+        if (htmlSelectedNodesTable == null) {
+            htmlSelectedNodesTable = "";
+        }
         
-
-        String[] values = request.getParameterValues("nodeid");
-        if (values != null) {
-            if (values.length > 0) {
-                
-
-                CorpusStructureDBImpl corpusDbConnection = (CorpusStructureDBImpl) this.getServletContext().getAttribute("corpusDbConnection");
-
-                if (corpusDbConnection == null) {
-
-                    logger.info("RrsIndex2: *************** corpusDbConnection == null ");
-
-                    ErrorRequest errorRequest = new ErrorRequest();
-
-                    errorRequest.setErrorFormFieldLabel("Corpus Database");
-                    errorRequest.setErrorMessage("Server is down");
-                    errorRequest.setErrorValue("");
-                    errorRequest.setErrorException(null);
-                    errorRequest.setErrorType("CORPUS_DATABASE_DOWN");
-
-                    errorsRequest.addError(errorRequest);
-                    errorsRequest.setErrorRecoverable(false);
-
-                    dispatchServlet(request, response, errorsRequest);
-                    return;
-                }
-
-                for (int i = 0; i < values.length; i++) {
-                    
-                    if (values[i] != null && !(values[i].equalsIgnoreCase(""))) {
-
-                        logger.info("RrsIndex2: *************** Param nodeId: " + values[i]);
-
-                        ImdiNode imdiNode = new ImdiNode();
-                        imdiNode.setImdiNodeIdWithPrefix(values[i]);
-                        String nodeIdWithPrefix = imdiNode.getImdiNodeIdWithPrefix();
-                        if (nodeIdWithPrefix != null) {
-                            try {
-                                imdiNode.setImdiNodeName(corpusDbConnection.getNode(nodeIdWithPrefix).getName());
-                                String nodeNameHtml = "paramNodeName_" + i;
-                                String nodeNameValue = imdiNode.getImdiNodeName();
-
-                                String nodeIdHtml = "nodeid";
-                                //paramNodeId_" + i;
-                                String nodeIdValue = values[i];
-
-                                htmlSelectedNodesTable += "<tr>";
-                                htmlSelectedNodesTable += "    <td> <input type=\"text\" name=\"" + nodeIdHtml + "\"  value = \"" + nodeIdValue + "\" readonly=\"readonly\" size=\"30\" /> ";
-                                htmlSelectedNodesTable += "    </td> ";
-                                htmlSelectedNodesTable += "    <td> <input type=\"text\" name=\"" + nodeNameHtml + "\"  value = \"" + nodeNameValue + "\" readonly=\"readonly\" size=\"50\" /> ";
-                                htmlSelectedNodesTable += "    </td> ";
-                                htmlSelectedNodesTable += " </tr> ";
-
-
-
-                            } catch (UnknownNodeException ex) {
-                                ErrorRequest errorRequest = new ErrorRequest();
-
-                                errorRequest.setErrorFormFieldLabel("Browser selected: Node Id");
-                                errorRequest.setErrorValue(values[i]);
-                                errorRequest.setErrorMessage("Invalid NodeId");
-                                errorRequest.setErrorException("mpi.corpusstructure.UnknownNodeException");
-                                errorRequest.setErrorType("INVALID_NODE_ID");
-
-                                errorsRequest.addError(errorRequest);
-                                errorsRequest.setErrorFromBrowser(true);
-                            }
-
-
-                        } else {
-                            ErrorRequest errorRequest = new ErrorRequest();
-
-                            errorRequest.setErrorFormFieldLabel("Browser selected: Node Id");
-                            errorRequest.setErrorValue(values[i]);
-                            errorRequest.setErrorMessage("Invalid NodeId");
-                            errorRequest.setErrorException("mpi.corpusstructure.UnknownNodeException");
-                            errorRequest.setErrorType("INVALID_NODE_ID");
-
-                            errorsRequest.addError(errorRequest);
-                            errorsRequest.setErrorFromBrowser(true);
-                        }
-
-                    }
-                }
-            }
-        } else {
-            logger.info("RrsIndex2: no nodeIds");
+        if (RrsIndex.createNodesTable(request, response, corpusDbConnection, errorsRequest, htmlSelectedNodesTable)) {
+            RrsIndex.createCalendarDropdowns(request);
         }
 
-        Calendar cal = Calendar.getInstance();
-
-        PulldownGenerator datePullDownToday = new PulldownGenerator(cal);
-
-        String pulldownDayOfMonthToday = datePullDownToday.pulldownDayOfMonth("paramRequestFromDateDay");
-        String pulldownMonthOfYearToday = datePullDownToday.pulldownMonthOfYear("paramRequestFromDateMonth");
-        String pulldownYearToday = datePullDownToday.pulldownYear("paramRequestFromDateYear");
-
-        cal.set(cal.YEAR, cal.get(cal.YEAR) + 1);
-
-        int year = cal.get(cal.YEAR);
-        PulldownGenerator datePullDownEnd = new PulldownGenerator(cal);
-
-        String pulldownDayOfMonthEnd = datePullDownEnd.pulldownDayOfMonth("paramRequestToDateDay");
-        String pulldownMonthOfYearEnd = datePullDownEnd.pulldownMonthOfYear("paramRequestToDateMonth");
-        String pulldownYearEnd = datePullDownEnd.pulldownYear("paramRequestToDateYear", year - 1, year + 9);
-
-        request.setAttribute("pulldownDayOfMonthToday", pulldownDayOfMonthToday);
-        request.setAttribute("pulldownMonthOfYearToday", pulldownMonthOfYearToday);
-        request.setAttribute("pulldownYearToday", pulldownYearToday);
-
-        request.setAttribute("pulldownDayOfMonthEnd", pulldownDayOfMonthEnd);
-        request.setAttribute("pulldownMonthOfYearEnd", pulldownMonthOfYearEnd);
-        request.setAttribute("pulldownYearEnd", pulldownYearEnd);
-
-        //request.setAttribute("htmlSelectedNodesTable", request.getAttribute(htmlSelectedNodesTable));
-        //request.setAttribute("htmlSelectedNodesTable", htmlSelectedNodesTable);
-        //this.getServletContext().setAttribute("htmlSelectedNodesTable", htmlSelectedNodesTable);
-        //HttpSession session = request.getSession();
-        session.setAttribute("htmlSelectedNodesTable", htmlSelectedNodesTable);
-
-        dispatchServlet(request, response, errorsRequest);
-        return;
+        RrsIndex.dispatchServlet(request, response, errorsRequest);
     }
 
-    public void dispatchServlet(HttpServletRequest request, HttpServletResponse response, ErrorsRequest errorsRequest)
-            throws ServletException, IOException {
-
-        Logger logger = Logger.getLogger("RrsIndex2");
-        logger.setLevel(Level.INFO);
-
-        if (errorsRequest.getSize() > 0) {
-            if (errorsRequest.isErrorFromBrowser()) {
-                errorsRequest.setErrorsHtmlTable();
-                String htmlErrorTable = errorsRequest.getErrorsHtmlTable();
-                request.setAttribute("htmlErrorTable", htmlErrorTable);
-
-                RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/view/error/errorBrowser.jsp");
-                view.forward(request, response);
-                return;
-
-            } else {
-                errorsRequest.setErrorsHtmlTable();
-                String htmlErrorTable = errorsRequest.getErrorsHtmlTable();
-                request.setAttribute("htmlErrorTable", htmlErrorTable);
-
-                RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/view/error/errorUnknown.jsp");
-                view.forward(request, response);
-                return;
-            }
-
-
-        } else {
-            //String fromFirstPage = (String) request.getParameter("fromFirstPage");
-            //logger.info("fromFirstPage: " + fromFirstPage);
-
-            String uidFromShib = request.getRemoteUser();
-            //if (uidFromShib.equalsIgnoreCase("anonymous") && RrsUtil.isEmpty(fromFirstPage)) {
-            if (uidFromShib.equalsIgnoreCase("anonymous")) {
-
-                RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/view/page/index.jsp");
-                view.forward(request, response);
-            } else {
-
-                RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/view/page/index_2.jsp");
-                view.forward(request, response);
-            }
-        }
-    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** Handles the HTTP <code>GET</code> method.
      * @param request servlet request
      * @param response servlet response
      */
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
@@ -261,6 +74,7 @@ public class RrsIndex2 extends HttpServlet {
      * @param request servlet request
      * @param response servlet response
      */
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
@@ -268,8 +82,9 @@ public class RrsIndex2 extends HttpServlet {
 
     /** Returns a short description of the servlet.
      */
+    @Override
     public String getServletInfo() {
-        return "Resource Request System Start Servlet";
+        return "Resource Request System Start Servlet phase 2";
     }
     // </editor-fold>
 }
