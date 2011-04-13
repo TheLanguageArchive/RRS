533ca786cc178bd386cab962c2ed30f14bed9f68
Twan Goosen <t.goosen@gmail.com>
2011-04-13 09:48:04 +0000

    Fixed some code style issues in PullDownGenerator, and added dependency to findbugs.annotations so that an incorrect warning message in the pullDownYear method can be suppressed

--- src/main/java/nl/mpi/rrs/model/date/PulldownGenerator.java
+++ src/main/java/nl/mpi/rrs/model/date/PulldownGenerator.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package nl.mpi.rrs.model.date;
 
 /**
@@ -16,11 +15,11 @@ package nl.mpi.rrs.model.date;
 import java.util.Calendar;
 
 public class PulldownGenerator {
-    private int yearRange = 10;
-    Calendar cal;
-    
-    //private int monthFormat = SHORT;
 
+    private int yearRange = 10;
+    private final Calendar cal;
+
+    //private int monthFormat = SHORT;
     /** Creates a new instance of PulldownGenerator */
     public PulldownGenerator() {
         cal = Calendar.getInstance();
@@ -30,7 +29,6 @@ public class PulldownGenerator {
         this.cal = cal;
     }
 
-
     public static void main(String[] args) {
         PulldownGenerator me = new PulldownGenerator();
         me.test();
@@ -38,70 +36,45 @@ public class PulldownGenerator {
     }
 
     public void test() {
-        //this.setMonthFormat(cal.LONG);
+        System.out.println(this.pulldownDayOfMonth("myDay"));
+        System.out.println(this.pulldownMonthOfYear("myMonth"));
+        System.out.println(this.pulldownYear("myYear", 2005, 2010));
+
+        cal.set(Calendar.YEAR, cal.get(Calendar.YEAR) + 1);
 
         System.out.println(this.pulldownDayOfMonth("myDay"));
         System.out.println(this.pulldownMonthOfYear("myMonth"));
-        System.out.println(this.pulldownYear("myYear",2005,2010));
-
-        cal.set(cal.YEAR,cal.get(cal.YEAR) + 1);
-
-        System.out.println(this.pulldownDayOfMonth("myDay"));
-        System.out.println(this.pulldownMonthOfYear("myMonth"));
-        System.out.println(this.pulldownYear("myYear",2005,2010));
-
+        System.out.println(this.pulldownYear("myYear", 2005, 2010));
     }
 
     public String pulldownDayOfMonth(String name) {
-        int dayOfMonth = cal.get(cal.DAY_OF_MONTH);
+        final int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
 
-        String dayPullDown = "<select name=\"" + name + "\">" + "\n";
+        StringBuilder dayPullDown = new StringBuilder("<select name=\"" + name + "\">" + "\n");
 
-        for (int i=1; i<=31; i++) {
-            String option = "<option>";
-            if (i == dayOfMonth) {
-                option = "<option selected=\"selected\">";
-            }
-
-            dayPullDown += option + i + "</option>" + "\n";
-
+        for (int i = 1; i <= cal.getMaximum(Calendar.DAY_OF_MONTH); i++) {
+            String option = (i == dayOfMonth) ? "<option selected=\"selected\">" : "<option>";
+            dayPullDown.append(option).append(i).append("</option>" + "\n");
         }
+        dayPullDown.append("</select>" + "\n");
 
-        dayPullDown += "</select>" + "\n";
-
-        return dayPullDown;
+        return dayPullDown.toString();
     }
 
     public String pulldownMonthOfYear(String name) {
-        int monthOfYear = cal.get(cal.MONTH);
-        
-        String[] shortMonths = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-        
-        /*
-        String monthOfYearShortString = cal.getDisplayName(cal.MONTH,monthFormat,Locale.ENGLISH);
+        final int monthOfYear = cal.get(Calendar.MONTH);
+        final String[] shortMonths = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-        DateFormatSymbols d = DateFormatSymbols.getInstance(Locale.ENGLISH);
-        if (this.monthFormat == cal.SHORT) {
-            shortMonths = d.getShortMonths();
-        } else {
-            shortMonths = d.getMonths();
-        }
-         */
+        StringBuilder monthPullDown = new StringBuilder("<select name=\"" + name + "\">" + "\n");
 
-        String monthPullDown = "<select name=\"" + name + "\">" + "\n";
-
-        for (int i=0; i<=11; i++) {
-            String option = "<option>";
-            if (i == monthOfYear) {
-                option = "<option selected=\"selected\">";
-            }
-
-            monthPullDown += option + shortMonths[i] + "</option>" + "\n";
+        for (int i = 0; i <= shortMonths.length - 1; i++) {
+            String option = (i == monthOfYear) ? "<option selected=\"selected\">" : "<option>";
+            monthPullDown.append(option).append(shortMonths[i]).append("</option>" + "\n");
         }
 
-        monthPullDown += "</select>" + "\n";
+        monthPullDown.append("</select>\n");
 
-        return monthPullDown;
+        return monthPullDown.toString();
     }
 
     public String pulldownYear(String name) {
@@ -110,8 +83,9 @@ public class PulldownGenerator {
         return this.pulldownYear(name, fromYear, toYear);
     }
 
+    @edu.umd.cs.findbugs.annotations.SuppressWarnings(value="IL_INFINITE_LOOP",justification="Incorrectly classified as an infinite loop")
     public String pulldownYear(String name, int fromYear, int toYear) {
-        int year = cal.get(cal.YEAR);
+        int year = cal.get(Calendar.YEAR);
         if (fromYear == -1) {
             fromYear = year;
         }
@@ -119,37 +93,29 @@ public class PulldownGenerator {
         if (toYear == -1) {
             toYear = fromYear + yearRange;
         }
+        StringBuilder yearPullDown = new StringBuilder();
+        yearPullDown.append("<select name=\"").append(name).append("\">" + "\n");
 
-
-        String yearPullDown = "<select name=\"" + name + "\">" + "\n";
-
-        for (int i=fromYear; i<=toYear; i++) {
-            String option = "<option>";
-            if (i == year) {
-                option = "<option selected=\"selected\">";
-            }
-
-            yearPullDown += option + i + "</option>" + "\n";
-
+        for (int i = fromYear; i <= toYear; i++) {
+            String option = (i != year) ? "<option>" : "<option selected=\"selected\">";
+            yearPullDown.append(option).append(i).append("</option>\n");
         }
 
-        yearPullDown += "</select>" + "\n";
-
-        return yearPullDown;
+        yearPullDown.append("</select>\n");
+        return yearPullDown.toString();
     }
 
     /*
     public int getMonthFormat() {
-        return monthFormat;
+    return monthFormat;
     }
 
     public void setMonthFormat(int monthFormat) {
-        if (monthFormat == cal.SHORT || monthFormat == cal.LONG) {        
-            this.monthFormat = monthFormat;
-        }
+    if (monthFormat == cal.SHORT || monthFormat == cal.LONG) {
+    this.monthFormat = monthFormat;
     }
-    */
-
+    }
+     */
     public int getYearRange() {
         return yearRange;
     }
