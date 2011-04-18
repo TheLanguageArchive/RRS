c648c4711e35f15a2a5f82636ad46c8d4a260b98
Twan Goosen <t.goosen@gmail.com>
2011-04-18 14:18:51 +0000

    Lana authentication provider

---
+++ src/main/java/nl/mpi/rrs/authentication/LanaAuthenticationProvider.java
@@ -0,0 +1,28 @@
+package nl.mpi.rrs.authentication;
+
+import javax.servlet.http.HttpServletRequest;
+import mpi.lana2.auth.AuthFilter;
+import nl.mpi.rrs.model.user.RegistrationUser;
+
+/**
+ * Lana2 implementation of AuthenticationProvider
+ * @author Twan Goosen <twan.goosen@mpi.nl>
+ */
+public class LanaAuthenticationProvider implements AuthenticationProvider {
+
+    public String getLoggedInUser(HttpServletRequest request) {
+        return (String) request.getSession().getAttribute(AuthFilter.ATTRIBUTE_UID);
+    }
+
+    public boolean isUserLoggedIn(HttpServletRequest request) {
+        return null != request.getSession().getAttribute(AuthFilter.ATTRIBUTE_UID);
+    }
+
+    public String getIdentityProviderId(HttpServletRequest request) {
+        return "Lana2";
+    }
+
+    public RegistrationUser createRegistrationUser(HttpServletRequest request) {
+        return new RegistrationUser();
+    }
+}
