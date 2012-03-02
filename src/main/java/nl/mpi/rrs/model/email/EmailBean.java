8f001dcf587814b5e28eccef4df8e5bf04f1ded1
Twan Goosen <t.goosen@gmail.com>
2012-03-02 10:57:24 +0000

    Removed defaults from EmailBean

--- src/main/java/nl/mpi/rrs/model/email/EmailBean.java
+++ src/main/java/nl/mpi/rrs/model/email/EmailBean.java
@@ -1,6 +1,5 @@
 package nl.mpi.rrs.model.email;
 
-import java.util.MissingResourceException;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -17,27 +16,6 @@ import org.apache.commons.logging.LogFactory;
 public class EmailBean {
 
     private final static Log logger = LogFactory.getLog(EmailBean.class);
-    //defaults
-    private final static String DEFAULT_CONTENT = "Unknown content";
-    private final static String DEFAULT_SUBJECT = "Unknown subject";
-    private static String DEFAULT_SERVER = null;
-    private static String DEFAULT_TO = null;
-    private static String DEFAULT_FROM = null;
-
-    static {
-        try {
-            java.util.ResourceBundle bundle =
-                    java.util.ResourceBundle.getBundle("nl.mpi.rrs.model.email.mailDefaults");
-            DEFAULT_SERVER = bundle.getString("DEFAULT_SERVER");
-            DEFAULT_TO = bundle.getString("DEFAULT_TO");
-            DEFAULT_FROM = bundle.getString("DEFAULT_FROM");
-
-            logger.info("DEFAULT_SERVER: " + bundle.getString("DEFAULT_SERVER"));
-        } catch (MissingResourceException ex) {
-            logger.warn("Email defaults resource bundle is missing");
-        }
-
-    }
     //JavaBean properties
     private String smtpHost;
     private String to;
@@ -48,138 +26,138 @@ public class EmailBean {
 
     public void sendMessage() throws Exception {
 
-        Properties properties = System.getProperties();
+	Properties properties = System.getProperties();
 
-        //populate the 'Properties' object with the mail
-        //server address, so that the default 'Session'
-        //instance can use it.
-        properties.put("mail.smtp.host", smtpHost);
+	//populate the 'Properties' object with the mail
+	//server address, so that the default 'Session'
+	//instance can use it.
+	properties.put("mail.smtp.host", smtpHost);
 
-        Session session = Session.getDefaultInstance(properties);
+	Session session = Session.getDefaultInstance(properties);
 
-        Message mailMsg = new MimeMessage(session);//a new email message
+	Message mailMsg = new MimeMessage(session);//a new email message
 
-        InternetAddress[] addresses = null;
+	InternetAddress[] addresses = null;
 
-        try {
+	try {
 
 
-            if (to != null) {
+	    if (to != null) {
 
-                //throws 'AddressException' if the 'to' email address
-                //violates RFC822 syntax
-                addresses = InternetAddress.parse(to, false);
+		//throws 'AddressException' if the 'to' email address
+		//violates RFC822 syntax
+		addresses = InternetAddress.parse(to, false);
 
-                mailMsg.setRecipients(Message.RecipientType.TO, addresses);
+		mailMsg.setRecipients(Message.RecipientType.TO, addresses);
 
-                logger.debug("EmailBean.sendMessage to: " + to);
+		logger.debug("EmailBean.sendMessage to: " + to);
 
-            } else {
+	    } else {
 
-                throw new MessagingException(
-                        "The mail message requires a 'To' address.");
+		throw new MessagingException(
+			"The mail message requires a 'To' address.");
 
-            }
+	    }
 
-            if (cc != null) {
+	    if (cc != null) {
 
-                //throws 'AddressException' if the 'to' email address
-                //violates RFC822 syntax
-                addresses = InternetAddress.parse(cc, false);
+		//throws 'AddressException' if the 'to' email address
+		//violates RFC822 syntax
+		addresses = InternetAddress.parse(cc, false);
 
-                mailMsg.setRecipients(Message.RecipientType.CC, addresses);
+		mailMsg.setRecipients(Message.RecipientType.CC, addresses);
 
-                logger.debug("EmailBean.sendMessage cc: " + cc);
+		logger.debug("EmailBean.sendMessage cc: " + cc);
 
-            } else {
+	    } else {
 
-                logger.error("The mail message has no 'Cc' address.");
+		logger.error("The mail message has no 'Cc' address.");
 
-            }
+	    }
 
-            if (from != null) {
+	    if (from != null) {
 
-                mailMsg.setFrom(new InternetAddress(from));
-                logger.debug("EmailBean.sendMessage from: " + from);
+		mailMsg.setFrom(new InternetAddress(from));
+		logger.debug("EmailBean.sendMessage from: " + from);
 
-            } else {
+	    } else {
 
-                throw new MessagingException(
-                        "The mail message requires a valid 'From' address.");
+		throw new MessagingException(
+			"The mail message requires a valid 'From' address.");
 
-            }
+	    }
 
-            if (subject != null) {
-                mailMsg.setSubject(subject);
-            }
+	    if (subject != null) {
+		mailMsg.setSubject(subject);
+	    }
 
-            if (content != null) {
-                mailMsg.setText(content);
-            }
+	    if (content != null) {
+		mailMsg.setText(content);
+	    }
 
-            //Finally, send the mail message; throws a 'SendFailedException',
-            // com.sun.mail.smtp.SMTPAddressFailedException
-            //if any of the message's recipients have an invalid address
-            Transport.send(mailMsg);
+	    //Finally, send the mail message; throws a 'SendFailedException',
+	    // com.sun.mail.smtp.SMTPAddressFailedException
+	    //if any of the message's recipients have an invalid address
+	    Transport.send(mailMsg);
 
-        } catch (Exception exc) {
-            logger.error("Address exption from: " + from + " cc: " + cc);
+	} catch (Exception exc) {
+	    logger.error("Address exption from: " + from + " cc: " + cc);
 
-            throw exc;
+	    throw exc;
 
-        }
+	}
 
     }//sendMessage
 
     public void setSmtpHost(String host) {
-        if (RrsUtil.isNotEmpty(host)) {
-            this.smtpHost = host;
-        } else {
-            this.smtpHost = EmailBean.DEFAULT_SERVER;
-        }
+	if (RrsUtil.isNotEmpty(host)) {
+	    this.smtpHost = host;
+	} else {
+	    throw new IllegalArgumentException("SMTP host cannot be empty");
+	}
     }//setTo
 
     public void setTo(String to) {
-        if (RrsUtil.isNotEmpty(to)) {
-            this.to = to;
-        } else {
-            this.to = EmailBean.DEFAULT_TO;
-        }
+	if (RrsUtil.isNotEmpty(to)) {
+	    this.to = to;
+	} else {
+	    throw new IllegalArgumentException("E-mail 'to' cannot be empty");
+	}
     }//setTo
 
     public void setFrom(String from) {
-        if (RrsUtil.isNotEmpty(from)) {
-            this.from = from;
-        } else {
-            this.from = EmailBean.DEFAULT_FROM;
-        }
+	if (RrsUtil.isNotEmpty(from)) {
+	    this.from = from;
+	} else {
+	    throw new IllegalArgumentException("E-mail 'from' cannot be empty");
+	}
     }//setFrom
 
     public void setContent(String content) {
-        if (RrsUtil.isNotEmpty(content)) {
-            this.content = content;
-        } else {
-            this.content = EmailBean.DEFAULT_CONTENT;
-        }
+	if (RrsUtil.isNotEmpty(content)) {
+	    this.content = content;
+	} else {
+	    throw new IllegalArgumentException("E-mail content cannot be empty");
+	}
     }//setContent
 
     public void setSubject(String subject) {
-        if (RrsUtil.isNotEmpty(subject)) {
-            this.subject = subject;
-        } else {
-            this.subject = EmailBean.DEFAULT_SUBJECT;
-        }
+	if (RrsUtil.isNotEmpty(subject)) {
+	    this.subject = subject;
+	} else {
+	    throw new IllegalArgumentException("E-mail subject cannot be empty");
+	}
     }//setSubject
 
     public String getCc() {
-        return cc;
+	return cc;
     }
 
     public void setCc(String cc) {
-        if (RrsUtil.isNotEmpty(cc)) {
-            this.cc = cc;
-        } else {
-            this.cc = null;
-        }
+	if (RrsUtil.isNotEmpty(cc)) {
+	    this.cc = cc;
+	} else {
+	    this.cc = null;
+	}
     }
 }
