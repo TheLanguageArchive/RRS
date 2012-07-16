ba6681ac885341ece58756a2490c361c8eec47ef
Twan Goosen <t.goosen@gmail.com>
2012-07-16 12:51:12 +0000

    Removed reading of shibstate cookie (refs #2143)* Added code to read shibstate cookie (refs #2143)

--- src/main/java/nl/mpi/rrs/controller/RrsIndex.java
+++ src/main/java/nl/mpi/rrs/controller/RrsIndex.java
@@ -6,37 +6,51 @@
 package nl.mpi.rrs.controller;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
-
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import nl.mpi.corpusstructure.ArchiveObjectsDB;
 import nl.mpi.corpusstructure.CorpusStructureDB;
 import nl.mpi.corpusstructure.UnknownNodeException;
 import nl.mpi.rrs.RrsConstants;
+import nl.mpi.rrs.authentication.AuthenticationProvider;
 import nl.mpi.rrs.model.corpusdb.ImdiNode;
 import nl.mpi.rrs.model.date.PulldownGenerator;
 import nl.mpi.rrs.model.errors.ErrorRequest;
 import nl.mpi.rrs.model.errors.ErrorsRequest;
 import nl.mpi.rrs.model.user.UserGenerator;
-import nl.mpi.rrs.authentication.AuthenticationProvider;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
  * Show Resource Request Form
+ *
  * @author kees
  * @version
  */
 public class RrsIndex extends HttpServlet {
 
+    public static final String SHIBSTATE_COOKIE_PREFIX = "_shibstate";
     private static Log logger = LogFactory.getLog(RrsIndex.class);
 
-    /** Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+    /** Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
      * @param request servlet request
      * @param response servlet response
      */
@@ -55,74 +69,73 @@ public class RrsIndex extends HttpServlet {
 
 	ErrorsRequest errorsRequest = new ErrorsRequest();
 	CorpusStructureDB corpusDbConnection = (CorpusStructureDB) this.getServletContext().getAttribute(RrsConstants.CORPUS_DB_CONNECTION_ATTRIBUTE);
-	if (createNodesTable(request, response, corpusDbConnection, errorsRequest, "")) {
+	ArchiveObjectsDB archiveObjectsConnection = (ArchiveObjectsDB) this.getServletContext().getAttribute(RrsConstants.ARCHIVE_OBJECTS_DB_CONNECTION_ATTRIBUTE);
+	if (createNodesTable(request, response, corpusDbConnection, archiveObjectsConnection, errorsRequest, "")) {
 	    createCalendarDropdowns(request);
 	}
 	dispatchServlet(request, response, errorsRequest, getServletContext());
     }
 
-    static boolean createNodesTable(HttpServletRequest request, HttpServletResponse response, CorpusStructureDB corpusDbConnection, ErrorsRequest errorsRequest, String htmlSelectedNodesTable) throws IOException, ServletException {
+    static boolean createNodesTable(HttpServletRequest request, HttpServletResponse response, CorpusStructureDB corpusDbConnection, ArchiveObjectsDB archiveObjectsConnection, ErrorsRequest errorsRequest, String htmlSelectedNodesTable) throws IOException, ServletException {
 	logger.debug("RrsIndex2 Context: htmlSelectedNodesTable:" + htmlSelectedNodesTable);
 	if (htmlSelectedNodesTable == null) {
 	    htmlSelectedNodesTable = "";
 	}
 	if (htmlSelectedNodesTable.length() == 0) {
-	    String[] values = request.getParameterValues("nodeid");
-	    if (values != null) {
-		if (values.length > 0) {
-		    if (corpusDbConnection == null) {
-			logger.warn("RrsIndex: *************** corpusDbConnection == null ");
-			ErrorRequest errorRequest = new ErrorRequest();
-			errorRequest.setErrorFormFieldLabel("Corpus Database");
-			errorRequest.setErrorMessage("Server is down");
-			errorRequest.setErrorValue("");
-			errorRequest.setErrorException(null);
-			errorRequest.setErrorType("CORPUS_DATABASE_DOWN");
-			errorsRequest.addError(errorRequest);
-			errorsRequest.setErrorRecoverable(false);
-			return false;
-		    }
+	    List<String> values = getNodeIds(request, archiveObjectsConnection);
+	    if (values.size() > 0) {
+		if (corpusDbConnection == null) {
+		    logger.warn("RrsIndex: *************** corpusDbConnection == null ");
+		    ErrorRequest errorRequest = new ErrorRequest();
+		    errorRequest.setErrorFormFieldLabel("Corpus Database");
+		    errorRequest.setErrorMessage("Server is down");
+		    errorRequest.setErrorValue("");
+		    errorRequest.setErrorException(null);
+		    errorRequest.setErrorType("CORPUS_DATABASE_DOWN");
+		    errorsRequest.addError(errorRequest);
+		    errorsRequest.setErrorRecoverable(false);
+		    return false;
+		}
 
-		    for (int i = 0; i < values.length; i++) {
-			if (values[i] != null && !(values[i].equalsIgnoreCase(""))) {
-			    logger.debug("RrsIndex: *************** Param nodeId: " + values[i]);
-			    ImdiNode imdiNode = new ImdiNode();
-			    imdiNode.setImdiNodeIdWithPrefix(values[i]);
-			    String nodeIdWithPrefix = imdiNode.getImdiNodeIdWithPrefix();
-			    if (nodeIdWithPrefix != null) {
-				try {
-				    imdiNode.setImdiNodeName(corpusDbConnection.getNode(nodeIdWithPrefix).getName());
-				    String nodeNameHtml = "paramNodeName_" + i;
-				    String nodeNameValue = imdiNode.getImdiNodeName();
-				    String nodeIdHtml = "nodeid";
-				    //paramNodeId_" + i;
-				    String nodeIdValue = values[i];
-				    htmlSelectedNodesTable += "<tr>";
-				    htmlSelectedNodesTable += "    <td> <input type=\"text\" name=\"" + nodeIdHtml + "\"  value = \"" + nodeIdValue + "\" readonly=\"readonly\" size=\"30\" /> ";
-				    htmlSelectedNodesTable += "    </td> ";
-				    htmlSelectedNodesTable += "    <td> <input type=\"text\" name=\"" + nodeNameHtml + "\"  value = \"" + nodeNameValue + "\" readonly=\"readonly\" size=\"50\" /> ";
-				    htmlSelectedNodesTable += "    </td> ";
-				    htmlSelectedNodesTable += " </tr> ";
-				} catch (UnknownNodeException ex) {
-				    ErrorRequest errorRequest = new ErrorRequest();
-				    errorRequest.setErrorFormFieldLabel("Browser selected: Node Id");
-				    errorRequest.setErrorValue(values[i]);
-				    errorRequest.setErrorMessage("Invalid NodeId");
-				    errorRequest.setErrorException("mpi.corpusstructure.UnknownNodeException");
-				    errorRequest.setErrorType("INVALID_NODE_ID");
-				    errorsRequest.addError(errorRequest);
-				    errorsRequest.setErrorFromBrowser(true);
-				}
-			    } else {
+		//for (int i = 0; i < values.length; i++) {
+		int i = 0;
+		for (String nodeId : values) {
+		    if (nodeId != null && !(nodeId.equalsIgnoreCase(""))) {
+			logger.debug("RrsIndex: *************** Param nodeId: " + nodeId);
+			ImdiNode imdiNode = new ImdiNode();
+			imdiNode.setImdiNodeIdWithPrefix(nodeId);
+			String nodeIdWithPrefix = imdiNode.getImdiNodeIdWithPrefix();
+			if (nodeIdWithPrefix != null) {
+			    try {
+				imdiNode.setImdiNodeName(corpusDbConnection.getNode(nodeIdWithPrefix).getName());
+				String nodeNameHtml = "paramNodeName_" + i++;
+				String nodeNameValue = imdiNode.getImdiNodeName();
+				String nodeIdHtml = "nodeid";
+				htmlSelectedNodesTable += "<tr>";
+				htmlSelectedNodesTable += "    <td> <input type=\"text\" name=\"" + nodeIdHtml + "\"  value = \"" + nodeId + "\" readonly=\"readonly\" size=\"30\" /> ";
+				htmlSelectedNodesTable += "    </td> ";
+				htmlSelectedNodesTable += "    <td> <input type=\"text\" name=\"" + nodeNameHtml + "\"  value = \"" + nodeNameValue + "\" readonly=\"readonly\" size=\"50\" /> ";
+				htmlSelectedNodesTable += "    </td> ";
+				htmlSelectedNodesTable += " </tr> ";
+			    } catch (UnknownNodeException ex) {
 				ErrorRequest errorRequest = new ErrorRequest();
 				errorRequest.setErrorFormFieldLabel("Browser selected: Node Id");
-				errorRequest.setErrorValue(values[i]);
+				errorRequest.setErrorValue(nodeId);
 				errorRequest.setErrorMessage("Invalid NodeId");
 				errorRequest.setErrorException("mpi.corpusstructure.UnknownNodeException");
 				errorRequest.setErrorType("INVALID_NODE_ID");
 				errorsRequest.addError(errorRequest);
 				errorsRequest.setErrorFromBrowser(true);
 			    }
+			} else {
+			    ErrorRequest errorRequest = new ErrorRequest();
+			    errorRequest.setErrorFormFieldLabel("Browser selected: Node Id");
+			    errorRequest.setErrorValue(nodeId);
+			    errorRequest.setErrorMessage("Invalid NodeId");
+			    errorRequest.setErrorException("mpi.corpusstructure.UnknownNodeException");
+			    errorRequest.setErrorType("INVALID_NODE_ID");
+			    errorsRequest.addError(errorRequest);
+			    errorsRequest.setErrorFromBrowser(true);
 			}
 		    }
 		}
@@ -200,8 +213,49 @@ public class RrsIndex extends HttpServlet {
 	}
     }
 
+    private static List<String> getNodeIds(HttpServletRequest request, ArchiveObjectsDB archiveObjectsConnection) {
+	List<String> values = new ArrayList<String>();
+
+	final String[] nodeIdValues = request.getParameterValues("nodeid");
+	if (nodeIdValues != null) {
+	    values.addAll(Arrays.asList(nodeIdValues));
+	}
+	findNodeIdsInShibstate(values, request, archiveObjectsConnection);
+
+	return values;
+    }
+
+    private static void findNodeIdsInShibstate(List<String> values, HttpServletRequest request, ArchiveObjectsDB archiveObjectsConnection) {
+	for (Cookie cookie : request.getCookies()) {
+	    if (cookie.getName().startsWith(SHIBSTATE_COOKIE_PREFIX)) {
+		logger.debug("Found shibstate cookie, trying to extract node id");
+		final String resourceURLString = cookie.getValue();
+		if (resourceURLString != null) {
+		    try {
+			String decodedResourceURLString = URLDecoder.decode(resourceURLString, "UTF-8");
+			logger.debug("URL in shibstate: " + resourceURLString);
+			URI resourceURI = new URL(decodedResourceURLString).toURI();
+			String nodeId = archiveObjectsConnection.getObjectId(resourceURI);
+			if (nodeId != null) {
+			    logger.debug("URL in shibstate translates to node id: " + resourceURLString);
+			    values.add(nodeId);
+			}
+		    } catch (MalformedURLException ex) {
+			logger.warn("Skipping potential URI in shibstate header", ex);
+		    } catch (URISyntaxException ex) {
+			logger.warn("Skipping potential URI in shibstate header", ex);
+		    } catch (UnsupportedEncodingException ex) {
+			logger.warn("Skipping potential URI in shibstate header", ex);
+		    }
+		}
+	    }
+	}
+    }
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** Handles the HTTP <code>GET</code> method.
+    /** Handles the HTTP
+     * <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      */
@@ -211,7 +265,9 @@ public class RrsIndex extends HttpServlet {
 	processRequest(request, response);
     }
 
-    /** Handles the HTTP <code>POST</code> method.
+    /** Handles the HTTP
+     * <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      */
